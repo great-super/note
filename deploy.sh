74fd4370594971d872b43b6d910acd3ccbc4d00e
poker <poker@xpoker.com>
2019-10-07 16:23:47 +0800

    d

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
+git pull
+git add docs
+git commit docs -am 'd'
+git push docs
 npx vuepress build docs
 cd docs/.vuepress/dist/
 git init
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/great-super/note.git master:gh-pages
+echo "发布成功~~"
+exit
